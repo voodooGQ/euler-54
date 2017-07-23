4cdd9d7ff67728c579de9ac1ce2ba7afdc04b6be
Shane Smith <voodoogq@gmail.com>
2017-07-23 16:30:02 -0500

    Adding main run script

---
+++ run.rb
@@ -0,0 +1,10 @@
+Dir.glob('./lib/**/*.rb').each{|file| require file}
+
+games = File.open('./data/poker.txt').map do |line|
+  Poker.new(line).declare_winner
+end
+
+total = 0
+games.each{|player, message| total += 1 if player == 1 }
+
+puts "Player 1 won a total of #{total} games"
